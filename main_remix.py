6fef69a61fd9a31b3c2bd287f7308834171528d5
AiGentsy <wadepapas@gmail.com>
2025-06-15 17:29:07 -0400

    Update main_remix.py

--- main_remix.py
+++ main_remix.py
@@ -14,8 +14,7 @@ app.add_middleware(
     allow_origins=["*"],  # Or restrict to ["https://aigentsy.com"]
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
-    allow_methods=["*"]
+    allow_headers=["*"]
 )
 
 @app.post("/agent")
